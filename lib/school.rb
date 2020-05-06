0d6db082b2a150f50b575da1c8767d48f86534ce
Gaby Mendez <gmendez90@gmail.com>
2020-05-06 15:18:17 -0400

    Complete iteration 2

--- lib/school.rb
+++ lib/school.rb
@@ -7,4 +7,20 @@ class School
     @student_names = []
   end
 
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    # add hours_in_school_day to start_time
+    # return in 24hr format
+    finish = @start_time.delete(":00").to_i + @hours_in_school_day
+
+    finish.to_s + ":00"
+    # I know this doesn't work if
+    # classes DIDN'T start at the top
+    # of the hour (00), but that's
+    # for a later iteration :)
+  end
+
 end
